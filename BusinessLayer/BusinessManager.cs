66a7ab9ab2f03d9f8729210451a57bb575c545b6
Gabriel Lundberg Puglia <s174099@student.hb.se>
2020-08-16 20:54:39 +0200

    asdasd

--- BusinessLayer/BusinessManager.cs
+++ BusinessLayer/BusinessManager.cs
@@ -85,14 +85,115 @@ namespace BusinessLayer
             }
             return false;
         }
-        public void CreateSendList(string Name)
+        public void CreateSendList(string Name) //form4
         {
             Employee Employee = unitOfWork.Employees.Get(PersonOnline.ID);
             SendList NewSendList = new SendList();
             NewSendList.Name = Name;
             NewSendList.Employee = Employee;
             unitOfWork.SendLists.Add(NewSendList);
+            unitOfWork.Save();
+        }
+        public void CreateActivity(Activity NewActivity) // form4
+        {
+            NewActivity.Employee = unitOfWork.Employees.Get(PersonOnline.ID);
+            unitOfWork.Activities.Add(NewActivity);
+            unitOfWork.Save();
         }
 
+        //Change
+        public void ChangeAlumnus(IAlumnus alumnus) //form 2
+        {
+            Alumnus alumnusX = unitOfWork.Alumnuses.Get(PersonOnline.ID);
+            alumnusX.Fname = alumnus.Fname;
+            alumnusX.Lname = alumnus.Lname;
+            alumnusX.PersonCode = alumnus.PersonCode;
+            alumnusX.Email = alumnus.Email;
+            alumnusX.Password = alumnus.Password;
+            alumnusX.Qualification = alumnus.Qualification;
+            alumnusX.ExamDate = alumnus.ExamDate;
+            unitOfWork.Save();
+        }
+        public void ChangeAlumnus2(IAlumnus alumnus, string PersonCode) // form4
+        {
+            Alumnus alumnusX = unitOfWork.Alumnuses.GetAlumnus(PersonCode);
+            alumnusX.Fname = alumnus.Fname;
+            alumnusX.Lname = alumnus.Lname;
+            alumnusX.PersonCode = alumnus.PersonCode;
+            alumnusX.Email = alumnus.Email;
+            alumnusX.Password = alumnus.Password;
+            alumnusX.Qualification = alumnus.Qualification;
+            alumnusX.ExamDate = alumnus.ExamDate;
+            unitOfWork.Save();
+        }
+        public void ChangeActivity(IActivity activity) // form 4
+        {
+            Activity activityX = unitOfWork.Activities.Get(activity.ActivityID);
+            activityX.Event = activity.Event;
+            activityX.Adress = activity.Adress;
+            activityX.Category = activity.Category;
+            activityX.Date = activity.Date;
+            activityX.Employee = unitOfWork.Employees.Get(PersonOnline.ID);
+            unitOfWork.Save();
+        }
+
+        //Delete
+        public void DeleteAlumnus() //form 3
+        {
+            Alumnus alumnusX = unitOfWork.Alumnuses.Get(PersonOnline.ID);
+            unitOfWork.Alumnuses.Remover(alumnusX);
+            unitOfWork.Save();
+        }
+        public void DeleteAlumnus2(string PersonCode) //form 4
+        {
+            Alumnus alumnusX = unitOfWork.Alumnuses.GetAlumnus(PersonCode);
+            unitOfWork.Alumnuses.Remover(alumnusX);
+            unitOfWork.Save();
+        }
+        public void DeleteSendList(int SendListId) //form 4
+        {
+            SendList sendListX = unitOfWork.SendLists.Get(SendListId);
+            unitOfWork.SendLists.Remover(sendListX);
+            unitOfWork.Save();
+        }
+        public void DeleteActivity(int ActivityId) //form 4
+        {
+            unitOfWork.Activities.Remover(unitOfWork.Activities.Get(ActivityId));
+            unitOfWork.Save();
+        }
+
+        // Add booking and SendList
+        public bool CreateBooking(int ActivityId) //form 3
+        {
+            Alumnus alumnusX = unitOfWork.Alumnuses.GetAlumnusActivity(PersonOnline.ID);
+            Activity activityX = unitOfWork.Activities.Get(ActivityId);
+            if(!alumnusX.Activities.Contains(activityX))
+            {
+                alumnusX.Activities.Add(activityX);
+                unitOfWork.Save();
+                return true;
+            }
+            return false;
+        }
+        public void AddAlumnusSenList(int SendListId, string PersonCode) //form 4
+        {
+            Alumnus alumnusX = unitOfWork.Alumnuses.GetAlumnus(PersonCode);
+            unitOfWork.SendLists.Get(SendListId).Alumnuses.Add(alumnusX);
+            unitOfWork.Save();
+        }
+        
+        // Remove objekt
+        public void RemoveAlumnusActivity(int ActivityId, string PersonCode) //form 4
+        {
+            Alumnus alumnusX = unitOfWork.Alumnuses.GetAlumnus(PersonCode);
+            unitOfWork.Activities.Get(ActivityId).Alumnuses.Remove(alumnusX);
+            unitOfWork.Save();
+        }
+        public void RemoveAlumnusSendList(int SendListId, string PersonCode)
+        {
+            Alumnus alumnusX = unitOfWork.Alumnuses.GetAlumnus(PersonCode);
+            unitOfWork.SendLists.Get(SendListId).Alumnuses.Remove(alumnusX);
+            unitOfWork.Save();
+        }
     }
 }
