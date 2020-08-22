ab6c494a4c0c288058c97d1098bf9010e894b068
Gabriel Lundberg Puglia <s174099@student.hb.se>
2020-08-22 16:40:26 +0200

    asdasd

--- GUI/Form3.cs
+++ GUI/Form3.cs
@@ -21,20 +21,77 @@ namespace GUI
         {
             InitializeComponent();
             Message();
+            ShowActivitis();
+            ShowBookedActivitis();
         }
 
+        public void ShowActivitis()
+        {
+            listView1.Items.Clear();
+            listView1.Columns.Clear();
+            listView1.Columns.Add("ID").Width = 25;
+            listView1.Columns.Add("Event").Width = 100;
+            listView1.Columns.Add("Address").Width = 150;
+            listView1.Columns.Add("Date").Width = 100;
+            listView1.Columns.Add("Category").Width = 50;
+
+            foreach(Activity activity in BusinessManager.GetAllActivities())
+            {
+                listView1.Items.Add(new ListViewItem(new string[] { activity.ActivityID.ToString(), activity.Event, activity.Adress, activity.Date.ToString(), activity.Category }));
+            }
+        }
+
+        public void ShowBookedActivitis()
+        {
+            listView2.Items.Clear();
+            listView2.Columns.Clear();
+            listView2.Columns.Add("ID").Width = 25;
+            listView2.Columns.Add("Event").Width = 150;
+            listView2.Columns.Add("Address").Width = 200;
+            listView2.Columns.Add("Date").Width = 150;
+            listView2.Columns.Add("Category").Width = 100;
+
+            foreach (Activity activity in BusinessManager.GetBookedActivities())
+            {
+                listView2.Items.Add(new ListViewItem(new string[] { activity.ActivityID.ToString(), activity.Event, activity.Adress, activity.Date.ToString(), activity.Category }));
+            }
+        }
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (listView1.SelectedItems.Count !=0)
+            {
+                bool Confirmed = BusinessManager.CreateBooking(int.Parse(listView1.SelectedItems[0].Text));
+                if (Confirmed == true)
+                {
+                    MessageBox.Show("Thank you for your booking, the booking is now completed");
+                    ShowBookedActivitis();
+                }
+                else
+                {
+                    MessageBox.Show("You've already made the booking before.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("You need to select an activity to be able to book");
+            }
         }
         private void Message()
         {
-            label3.Text = $"Welcome {BusinessManager.GetAlumnusOnline().Fname + " " + BusinessManager.GetAlumnusOnline().Lname}";
+            label3.Text = $"{BusinessManager.GetAlumnusOnline().Fname + " " + BusinessManager.GetAlumnusOnline().Lname}";
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
+            Form2 Change = new Form2();
+            Change.Text = "Change";
+            Change.ShowDialog();
+            BusinessManager = new BusinessManager();
+        }
 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
