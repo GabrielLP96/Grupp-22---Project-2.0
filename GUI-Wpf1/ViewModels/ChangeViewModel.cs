d2b8c06c0f0c5cd5b56feede8a7fdd9d4c0985b9
Gabriel Lundberg Puglia <s174099@student.hb.se>
2020-08-24 11:53:49 +0200

    sadasd

--- GUI-Wpf1/ViewModels/ChangeViewModel.cs
+++ GUI-Wpf1/ViewModels/ChangeViewModel.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessEntity.ClassModels;
+using DataLayer;
+using GUI_Wpf1.Views;
 
 namespace GUI_Wpf1.ViewModels
 {
-    class ChangeViewModel
+    class ChangeViewModel : NotifyPropertyChanged
     {
+        public static Alumnus AlumnusOnline;
+        private UnitOfWork UnitOfWork = new UnitOfWork(new DataContext());
+        private ChangeView view;
+
+        public MainCommand Change { get; }
+        public MainCommand Cancel { get; }
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string PersonCode { get; set; }
+
     }
 }
