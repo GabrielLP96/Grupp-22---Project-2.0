9ce123162124b4d95ec03235cf2b02fe17610653
Gabriel Lundberg Puglia <s174099@student.hb.se>
2020-08-24 11:03:10 +0200

    sadasd

--- GUI-Wpf1/ViewModels/AlumnusViewModel.cs
+++ GUI-Wpf1/ViewModels/AlumnusViewModel.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using 
 
 namespace GUI_Wpf1.ViewModels
 {
-    class AlumnusViewModel
+    class AlumnusViewModel : NotifyPropertyChanged
     {
-
+        public static 
     }
 }
